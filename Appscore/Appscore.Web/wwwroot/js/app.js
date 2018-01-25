5d6039dc1d38618094f571f6d8eec31c7be14da7
VeritasSoftware <shantanu_negi@hotmail.com>
2018-01-26 05:48:49 +1100

    display Level in advanced search result grid.

--- Appscore/Appscore.Web/wwwroot/js/app.js
+++ Appscore/Appscore.Web/wwwroot/js/app.js
@@ -4,13 +4,15 @@ app.directive("displaySearchResults", function ($parse) {
     return {
         restrict: 'EA',
         scope: {            
-            results: '='
+            results: '=',
+            advanced: '='
         },
         template: '<div>\
             <table border="1" ng-if="results.length > 0" style="width:100%">\
                 <thead>\
                     <tr style="background-color:olive; color:white">\
                         <td>ID</td>\
+                        <td ng-if="advanced">LEVEL</td>\
                         <td>NAME</td>\
                         <td>GENDER</td>\
                         <td>BIRTHPLACE</td>\
@@ -19,6 +21,7 @@ app.directive("displaySearchResults", function ($parse) {
                 <tbody>\
                     <tr ng-repeat="r in results">\
                         <td>{{ r.id }}</td>\
+                        <td ng-if="advanced">{{ r.level }}</td>\
                         <td>{{ r.name }}</td>\
                         <td>{{ r.gender == 2 ? \'Female\' : \'Male\' }}</td>\
                         <td>{{ r.birthPlace }}</td>\
